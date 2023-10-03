cfa4bbbec05604b713db42ef8ddd78b63efefede
Mikhail Pankin <s02180496@gse.cs.msu.ru>
2023-10-04 00:14:18 +0300

    Works with any locale

--- 03_Text/randomizer.sh
+++ 03_Text/randomizer.sh
@@ -11,22 +11,21 @@ result=""
 
 while read line; do
     for (( i=0; i<${#line}; i++ )); do
-        result="$result$(printf "%d" "'${line:i:1}") $line_num $i \n"
+        result="$result$(printf "${line:i:1}" | base64) $line_num $i \n"
     done
 
     line_num=$((line_num+1))
 done
 
 
-shuffeled=$(/bin/echo -ne $result | shuf)
+shuffeled=$(printf "$result" | shuf)
 
 tput clear
 while read line; do
     read a b c <<< "$line"
 
-
     tput cup $b $c
-    printf "\\x$(printf "%x" $a)\n"
+    printf "$a" | base64 -d
     sleep $1
 done <<< "$shuffeled"
 
