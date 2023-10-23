625d0ae4eba8f06b0cfb4ecb4572b908ce1770ec
Mikhail Pankin <s02180496@gse.cs.msu.ru>
2023-10-23 23:31:40 +0300

    Works!!

--- 05_Regexps/main.c
+++ 05_Regexps/main.c
@@ -1,24 +1,63 @@
 #include <regex.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#define GREEN "\033[0;32m"
+#define END "\033[0m"
+
 enum {
     ERR_BUFF_SIZE = 1024,
+    MAX_MATCH = 128,
 };
 
+bool use_color = true;
+
 void print_error(int errcode, regex_t *regex) {
-    char *errbuf = malloc(ERR_BUFF_SIZE);
+    char errbuf[ERR_BUFF_SIZE];
     regerror(errcode, regex, errbuf, ERR_BUFF_SIZE);
 
     printf("Error: %s\n", errbuf);
-    free(errbuf);
+}
+
+void replace_matches(char *string, char *substitution, regmatch_t *matches) {
+    printf("%.*s", matches[0].rm_so, string);
+
+    if (use_color) {
+        printf(GREEN);
+    }
+
+    while (*substitution) {
+        if (*substitution != '\\') {
+            printf("%c", *substitution);
+            substitution++;
+        } else {
+            int i = 0, shift = 0;
+            sscanf(substitution, "\\%d%n", &i, &shift);
+
+            printf("%.*s", matches[i].rm_eo - matches[i].rm_so,
+                   string + matches[i].rm_so);
+
+            substitution += shift;
+        }
+    }
+
+    if (use_color) {
+        printf(END);
+    }
+    printf("%s\n", string + matches[0].rm_eo);
 }
 
 int main(int argc, char *argv[]) {
     if (argc < 4) {
-        printf("Usage: %s <regexp> <substitution> <string>\n", argv[0]);
+        printf("Usage: %s <regexp> <substitution> <string> [-nc]\n", argv[0]);
+        printf("Optional: -nc (no color)\n");
         return 1;
+    }
+
+    if (argc > 4 && strcmp(argv[4], "-nc") == 0) {
+        use_color = false;
     }
 
     char *regexp = argv[1];
@@ -32,17 +71,15 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    regmatch_t match[1];
+    regmatch_t matches[MAX_MATCH];
 
-    errcode = regexec(&regex, string, 1, match, 0);
+    errcode = regexec(&regex, string, MAX_MATCH, matches, 0);
     if (errcode) {
         printf("%s\n", string);
         return 0;
     }
 
-    memset(string + match->rm_so, 0, match->rm_eo - match->rm_so);
-
-    printf("%s%s%s\n", string, substitution, string + match->rm_eo);
+    replace_matches(string, substitution, matches);
 
     regfree(&regex);
 }
