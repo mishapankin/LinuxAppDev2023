81ae662ad6d41562ca4e164d936275cb942e32b8
Mikhail Pankin <s02180496@gse.cs.msu.ru>
2023-11-21 22:54:53 +0300

    revert main.c

--- 09_PatchDiff/main.c
+++ 09_PatchDiff/main.c
@@ -4,74 +4,55 @@
 
 int steps[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
-int size = 13;
-char wall = '#';
-char path = '.';
-
 char get(int x, int y, char *labyrinth) {
-    return labyrinth[x * size + y];
+    return labyrinth[x * 13 + y];
 }
 
 char set(int x, int y, char *labyrinth, char value) {
-    labyrinth[x * size + y] = value;
+    labyrinth[x * 13 + y] = value;
 }
 
 void fill_labyrinth(int x, int y, char *labyrinth) {
     unsigned been = 0;
     for (int i = 0; i < 4;) {
-        unsigned step = rand() % 4;
+        int step = rand() % 4;
 
         if (been & (1 << step)) {
+            been |= 1 << step;
             continue;
         }
-        been |= 1 << step;
         ++i;
 
         int new_x = x + steps[step][0] * 2;
         int new_y = y + steps[step][1] * 2;
 
-        if (new_x < 1 || new_x >= size - 1 || new_y < 1 || new_y >= size - 1) {
+        if (new_x < 0 || new_x >= 13 || new_y < 0 || new_y >= 13) {
             continue;
         }
 
-        if (get(new_x, new_y, labyrinth) == wall) {
-            set(x + steps[step][0], y + steps[step][1], labyrinth, path);
-            set(new_x, new_y, labyrinth, path);
+        if (get(new_x, new_y, labyrinth) == '#') {
+            set(x + steps[step][0], y + steps[step][1], labyrinth, '.');
+            set(new_x, new_y, labyrinth, '.');
             fill_labyrinth(new_x, new_y, labyrinth);
         }
     }
 }
 
-int main(int argc, char **argv) {
-    unsigned seed = time(NULL);
-    if (argc == 2) {
-        size = atoi(argv[1]);
-    }
-    if (argc == 3) {
-        path = argv[1][0];
-        wall = argv[1][1];
-        size = atoi(argv[2]);
-    }
-    if (argc == 4) {
-        seed = atoi(argv[1]);
-        path = argv[2][0];
-        wall = argv[2][1];
-        size = atoi(argv[3]);
-    }
-    srand(seed);
+int main() {
+    srand(time(NULL));
 
-    char *labyrinth = malloc(size * size * sizeof(char));
+    char *labyrinth = malloc(13 * 13 * sizeof(char));
 
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-            set(i, j, labyrinth, wall);
+    for (int i = 0; i < 13; ++i) {
+        for (int j = 0; j < 13; ++j) {
+            set(i, j, labyrinth, '#');
         }
     }
 
     fill_labyrinth(1, 1, labyrinth);
 
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
+    for (int i = 0; i < 13; ++i) {
+        for (int j = 0; j < 13; ++j) {
             printf("%c", get(i, j, labyrinth));
         }
         printf("\n");
